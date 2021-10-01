ae561959632af91089be52b27f72f8291959949c
Deluria, Neil John <neil.john.deluria@accenture.com>
2021-10-01 11:19:47 +0800

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
- const weatherForm = document.querySelector('form')
+const weatherForm = document.querySelector('form')
  const search = document.querySelector('input')
  const message1 = document.querySelector('#message-1')
  const message2 = document.querySelector('#message-2')
@@ -11,7 +11,7 @@
         message1.textContent = "Loading...."
         message2.textContent = ''
 
-        fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+        fetch('/weather?address='+ location).then((response) => {
      response.json().then((data) => {
          if(data.error){
              message1.textContent= ''
--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ const forecast = require('./utils/forecast')
 
 
 const app = express();
+const port = proces.env.PORT || 3000
+
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../public/template/views')
-const partialsPath = path.join(__dirname, '../public/template/partials')
+const viewsPath = path.join(__dirname, '../template/views')
+const partialsPath = path.join(__dirname, '../template/partials')
 
 app.set('view engine' , 'hbs')
 app.set('views', viewsPath)
@@ -82,6 +84,6 @@ app.get('*' , (req,res) => {
         errormessage: '404 page'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port' + port)
 })
